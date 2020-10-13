e17c04dc3db6d8b548d3a5cb1efa21e10c6a207f
johannesegger <j.egger@posteo.at>
2020-10-13 20:08:07 +0200

    Use apt instead of apt-get

--- raspi/setup.sh
+++ raspi/setup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install -y git
+sudo apt update
+sudo apt install -y git
 
 # Install Grafana (see https://grafana.com/docs/grafana/latest/installation/debian/)
-sudo apt-get install -y apt-transport-https
-sudo apt-get install -y software-properties-common wget
+sudo apt install -y apt-transport-https
+sudo apt install -y software-properties-common wget
 wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
 echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
-sudo apt-get update
-sudo apt-get install -y grafana
+sudo apt update
+sudo apt install -y grafana
 
 sudo systemctl daemon-reload
 sudo systemctl start grafana-server
