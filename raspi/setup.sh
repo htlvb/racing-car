6b4eeae18c9eec4a8ce413ba9951ddd0e1a63363
Johannes Egger <j.egger@posteo.at>
2020-10-13 21:50:24 +0100

    Create minimal dashboard[B

--- raspi/setup.sh
+++ raspi/setup.sh
@@ -3,28 +3,20 @@
 sudo apt update
 sudo apt install -y git
 
-# Install Grafana (see https://grafana.com/docs/grafana/latest/installation/debian/)
-sudo apt install -y apt-transport-https
-sudo apt install -y software-properties-common wget
-wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
-echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
-sudo apt update
-sudo apt install -y grafana
+# Set host name
+sudo hostnamectl set-hostname racerpi1
+# TODO sudo reboot now
 
-sudo systemctl daemon-reload
-sudo systemctl start grafana-server
-sudo systemctl status grafana-server
-sudo systemctl enable grafana-server.service
+# Install pip
+sudo apt install -y python3-pip
 
 # Install docker (see https://docs.docker.com/engine/install/debian/)
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
+# curl -fsSL https://get.docker.com -o get-docker.sh
+# sudo sh get-docker.sh
+curl -fsSL https://download.docker.com/linux/raspbian/gpg | sudo apt-key add -
+echo "deb [arch=armhf] https://download.docker.com/linux/raspbian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker-ce.list
+sudo apt update
+sudo apt install -y docker-ce
+sudo pip3 install docker-compose
 
-# Install pip and packages
-sudo apt install -y python3-pip
-pip3 install prometheus_client
-pip3 install Bluetin-Echo
-
-pushd `dirname "$0"`
-sudo docker run -d -p 9090:9090 -v "`pwd`/prometheus.yml:/etc/prometheus/prometheus.yml" --restart unless-stopped --network host prom/prometheus
-popd
+docker-compose up -d
