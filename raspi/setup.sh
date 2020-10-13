fb94d1559b8de7c3fde7603abca0444c12ef38f9
Johannes Egger <j.egger@posteo.at>
2020-10-13 19:08:02 +0200

    Add script to setup Raspi

---
+++ raspi/setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y git
+
+# Install Grafana (see https://grafana.com/docs/grafana/latest/installation/debian/)
+sudo apt-get install -y apt-transport-https
+sudo apt-get install -y software-properties-common wget
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+sudo apt-get update
+sudo apt-get install -y grafana
+
+sudo systemctl daemon-reload
+sudo systemctl start grafana-server
+sudo systemctl status grafana-server
+sudo systemctl enable grafana-server.service
+
+# Install docker (see https://docs.docker.com/engine/install/debian/)
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
+docker run --restart unless-stopped -p 9090:9090 -v ./prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus
