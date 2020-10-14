c6a6c8fb946349ac23476b1ed30b65dd6a7aac44
Johannes Egger <j.egger@posteo.at>
2020-10-14 09:28:45 +0100

    Fix changing host name; Add comments

--- raspi/init.sh
+++ raspi/init.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+# Set password
 sudo passwd pi
-sudo hostnamectl set-hostname racerpi1
-# TODO run `sudo reboot now`
 
+# Set host name
+sudo sed -i 's/raspberrypi/racerpi1/g' /etc/hosts
+sudo sed -i 's/raspberrypi/racerpi1/g' /etc/hostname
+sudo systemctl restart systemd-logind.service
+sudo hostnamectl --static --transient --pretty set-hostname racerpi1
+
+# Clone git repository
 sudo apt update
 sudo apt install -y git
 git clone https://github.com/htlvb/racing-car.git
