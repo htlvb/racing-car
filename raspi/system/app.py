d20c3e612dcab91f924be0219aabb272b1447d17
Johannes Egger <j.egger@posteo.at>
2020-10-15 11:47:37 +0100

    Fix system info job

--- raspi/system/app.py
+++ raspi/system/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from prometheus_client import start_http_server, Info
+from prometheus_client import start_http_server, Gauge, Info
 import time
 from datetime import datetime
 import signal,sys
@@ -13,31 +13,35 @@ import socket
 sendepause = 5
 
 infoMetric = Info('system', 'System information')
+cpuUsageMetric = Gauge('cpuUsage', 'CPU usage in %')
+freeRamMetric = Gauge('freeRam', 'Free RAM in GB')
+freeDiskSpaceMetric = Gauge('freeDiskSpace', 'Free disk space in GB')
+cpuTemperatureMetric = Gauge('cpuTemperature', 'CPU temperature in °C')
 
 start_http_server(8000)
 
 def handle_sigterm(*args):
-    sensor.stop()
     sys.exit()
 
 signal.signal(signal.SIGTERM, handle_sigterm)
 
 while True:
     info = {}
-    info['system-time'] = str(datetime.now())
+    info['systemTime'] = str(datetime.now())
     info['architecture'] = platform.architecture()[0]
-    info['os-version'] = platform.version()
+    info['osVersion'] = platform.version()
     info['hostname'] = platform.node()
     info['platform'] = platform.platform()
     info['processor'] = platform.processor()
-    info['boot-time'] = datetime.fromtimestamp(psutil.boot_time())
-    info['cpu-usage'] = psutil.cpu_percent()
-    info['free-ram'] = f'{round(psutil.virtual_memory().free / (1024.0 ** 3), 2)} GB'
-    info['free-disk-space'] = f'{round(psutil.disk_usage("/").free / (1024.0 ** 3), 2)} GB'
-    # info['ip-address-wifi'] = psutil.net_if_addrs()["wlan0"][0].address
-    # info['mac-address'] = psutil.net_if_addrs()["eth0"][0].address
-    info['cpu-temperature'] = psutil.sensors_temperatures()['cpu-thermal'][0].current
+    info['bootTime'] = str(datetime.fromtimestamp(psutil.boot_time()))
+    # info['ipAddressWifi'] = psutil.net_if_addrs()["wlan0"][0].address
+    # info['macAddress'] = psutil.net_if_addrs()["eth0"][0].address
     infoMetric.info(info)
+
+    cpuUsageMetric.set(psutil.cpu_percent())
+    freeRamMetric.set(psutil.virtual_memory().free / (1024.0 ** 3))
+    freeDiskSpaceMetric.set(psutil.disk_usage("/").free / (1024.0 ** 3))
+    cpuTemperatureMetric.set(psutil.sensors_temperatures()['cpu-thermal'][0].current)
 
     time.sleep(sendepause)
 
